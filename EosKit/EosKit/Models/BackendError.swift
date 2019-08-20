3e9ba3f3b2693b2786ecc8e475853df2bd555ad3
max <trickster77777@gmail.com>
2019-08-20 15:04:01 +0600

    Parse errors returned by EOS API - catch and translate errors with status 500 to custom BackendError

---
+++ EosKit/EosKit/Models/BackendError.swift
@@ -0,0 +1,9 @@
+public enum BackendError: Error {
+    case selfTransfer
+    case accountNotExist
+    case overdrawn
+    case precisionMismatch
+    case insufficientRam
+    case wrongContract
+    case unknown
+}
